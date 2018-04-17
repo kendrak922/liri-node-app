756292402c86f3ef1152fb7e42b8a29a4e53a146
Kendra Kyndberg <kendrak922@gmail.com>
2018-04-17 13:36:21 -0500

    initial setup

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+console.log(process.env)
+const keys = require("./keys.js");
+
+// const spotify = new Spotify(keys.spotify);
+// const client = new Twitter(keys.twitter);
+
+// console.log(spotify)
