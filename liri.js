ea6a536b3da2493ef0be1989d5a96d004715df35
Kendra Kyndberg <kendrak922@gmail.com>
2018-04-18 12:22:18 -0500

    tweet API

--- liri.js
+++ liri.js
@@ -1,9 +1,34 @@
-require("dotenv").config();
-
-console.log(process.env)
+const result = require("dotenv").config();
+if (result.error) {
+  throw result.error
+}
+const fs = require("fs");
 const keys = require("./keys.js");
+let Twitter = require('twitter');
+let client = new Twitter(keys.twitter);
+let params = {
+    screen_name: 'KendraKyndbergg'
+} && {
+    count: 20
+};
+let spotify = require('spotify');
 
-// const spotify = new Spotify(keys.spotify);
-// const client = new Twitter(keys.twitter);
+// let spotify = new Spotify(keys.spotify);
+//twitter API
+const getTweets = client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    for(var key in tweets) {
+        console.log(tweets[key].text);
+      }
+  }
+});
 
-// console.log(spotify)
+//spotify API
+// spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+//     if ( err ) {
+//         console.log('Error occurred: ' + err);
+//         return;
+//     }
+ 
+//     // Do something with 'data' 
+// });
