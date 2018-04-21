19ec8d9cbb6a5b9ce6ee477697526207a576a3df
Kendra Kyndberg <kendrak922@gmail.com>
2018-04-21 12:10:49 -0500

    added OMDB

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ if (result.error) {
   throw result.error
 }
 const fs = require("fs");
+const request = require('request');
 const keys = require("./keys.js");
 let Twitter = require('twitter');
 let client = new Twitter(keys.twitter);
@@ -17,40 +18,55 @@ var method = process.argv[2];
 var search = process.argv[3];
 
 //twitter API
-const getTweets = client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for(let key in tweets) {
-        // console.log(tweets[key].text);
-      }
+// const getTweets = client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     for(let key in tweets) {
+//         // console.log(tweets[key].text);
+//       }
+//   }
+// });
+
+//spotify API
+// const music = spotify.search({ type: 'track', query: search, limit: 1 }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     } else {
+//       for(let key in data) { 
+//         // console.log("\n-------------\n");
+//         //   console.log(
+//         //     `Artist:${data.tracks.items[0].artists[0].name}
+//         //     \nSong Title: ${data.tracks.items[0].name}
+//         //     \nPreview Link: ${data.tracks.items[0].album.href}
+//         //     \nAlbum: ${data.tracks.items[0].album.name}`)
+//         //     console.log("\n-------------\n");
+//         //       }
+//       }    
+//     });
+//end of spotify API
+//OMDB Request
+queryURL = `http://www.omdbapi.com/?t=${method}&y=&plot=short&apikey=trilogy`
+request(queryURL, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log("\n-------------\n");
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("\nReleased in: " + JSON.parse(body).Year);
+    console.log("\nIMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+    console.log("\nCountry: " + JSON.parse(body).Country);
+    console.log("\nLanguage: " + JSON.parse(body).Language);
+    console.log("\nPlot: " + JSON.parse(body).Plot);
+    console.log("\nStarring " + JSON.parse(body).Actors);
+    console.log("\n-------------");
+   
   }
 });
 
-//spotify API
-// const getTrack = spotify.request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-// .then(function(data) {
-//     for(let key in data) {
-//         console.log(data.artist[key].name)
-//       }
-// })
-// .catch(function(err) {
-//   console.error('Error occurred: ' + err); 
-// });
-
-
-
-const music = spotify.search({ type: 'track', query: search, limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    } else {
-      for(let key in data) { 
-        console.log("\n-------------\n");
-          console.log(
-            `Artist:${data.tracks.items[0].artists[0].name}
-            \nSong Title: ${data.tracks.items[0].name}
-            \nPreview Link: ${data.tracks.items[0].album.href}
-            \nAlbum: ${data.tracks.items[0].album.name}`)
-            console.log("\n-------------\n");
-              }
-      }    
-    });
     
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
